91fa08c474db2e57ebf1e916e30fc0c92385c445
rockyamdad <rocky.amdad@gmail.com>
2018-09-15 05:11:39 +0600

    project initilize

---
+++ app/User/Models/Test.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\User\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Test extends Model
+{
+    //
+}
---
+++ resources/views/index.php
@@ -0,0 +1,6 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <body>
+            <h2>Index page</h2>
+    </body>
+</html>
